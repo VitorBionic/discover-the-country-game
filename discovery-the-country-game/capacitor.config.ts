5d9b18d04348fe85e876c15f14066e381b7c1b43
Vitor Bionic <vitorbionic@hotmail.com>
2025-11-03 20:29:10 -0300

    Project Initialized

---
+++ discovery-the-country-game/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'discovery-the-country-game',
+  webDir: 'www'
+};
+
+export default config;
