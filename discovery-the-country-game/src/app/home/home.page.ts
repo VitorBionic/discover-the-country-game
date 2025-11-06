c9e96cf9af3f2ac58618b0fe59724a10a4ac107a
Vitor Bionic <vitorbionic@hotmail.com>
2025-11-06 14:35:23 -0300

    Home page created

--- discovery-the-country-game/src/app/home/home.page.ts
+++ discovery-the-country-game/src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption, IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption, IonButton],
 })
 export class HomePage {
+
+  dificulty: string = "easy";
+
   constructor() {}
+
+  startGame() {
+
+  }
 }
